394c6cc74de348aab401e9b2084dfa7763d49fb1
LornahMunene <munenelornah9@gmail.com>
2024-08-05 21:52:12 +0300

    Adss code to Service appointmentpage

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage'; // Adjust this path if needed
+import ServiceList from './components/Services/ServiceList';
+import ServiceAppointmentPage from './pages/ServiceAppointmentsPage'; // Ensure the correct path
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/servicing" element={<ServiceList />} />
+        <Route path="/service-appointment" element={<ServiceAppointmentPage />} />
       </Routes>
     </Router>
   );
