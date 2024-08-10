4ec75497612fb89cbcf119a98c96ffaf07ff622e
tubisaffo <tubisaffo@gmail.com>
2024-08-10 12:08:24 +0300

    added appointment table

--- server/models.py
+++ server/models.py
@@ -62,3 +62,14 @@ class Cart(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     image_url = db.Column(db.String(500), nullable=True)
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    appointment_date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    user = db.relationship('User', backref=db.backref('appointments', lazy=True))
+    service = db.relationship('Service', backref=db.backref('appointments', lazy=True))
